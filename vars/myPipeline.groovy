cbd2ffb3e9b7aa6da991ef213ab6c1e6c8800976
Ganes3g <157215403+Ganes3g@users.noreply.github.com>
2025-03-12 16:00:22 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,1 +1,38 @@
-
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage ('Git checkout'){
+                steps {
+                    script {
+                        git branch: 'main', url:'https://github.com/Ganes3g/Shared-Lib-Creation.git'
+                    }
+                }
+            }
+            stage ('Maven Build') {
+                steps {
+                    script {
+                        sh 'npm install'
+                    }
+                }
+            }
+            stage ('Sonarcube analysis') {
+                steps {
+                    script {
+                        def scannerHome = tool 'sonarscanner4'
+                        withSonarQubeEnv ('sonar-pro')
+                        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=node.js-app"
+                    }
+                }
+            }
+            stage ('Docker build') {
+                steps {
+                    script {
+                        sh 'docker build -t ganeshimage .'
+                        sh 'docker images'
+                    }
+                }
+            }
+        }
+    }
+}
